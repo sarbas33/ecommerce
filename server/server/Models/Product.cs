b47db268aa3d76027c4fd981536d9e21c662c324
sarbas33 <sarbas33@gmail.com>
2023-11-23 23:04:00 +0530

    Scaffold existing database which contains 'Products' table

---
+++ server/server/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace server.Models
+{
+    public partial class Product
+    {
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public int? Price { get; set; }
+        public string? Description { get; set; }
+        public int? Category { get; set; }
+        public int? QuantityAvailable { get; set; }
+        public int? Mrp { get; set; }
+    }
+}
---
+++ server/server/Models/ecommerceDBContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace server.Models
+{
+    public partial class ecommerceDBContext : DbContext
+    {
+        public ecommerceDBContext()
+        {
+        }
+
+        public ecommerceDBContext(DbContextOptions<ecommerceDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\local;Database=ecommerceDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("products");
+
+                entity.HasIndex(e => e.Name, "UQ__products__737584F65561DE76")
+                    .IsUnique();
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.Mrp).HasColumnName("MRP");
+
+                entity.Property(e => e.Name).HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
